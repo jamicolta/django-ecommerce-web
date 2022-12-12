339109adefcb135d580a6edbb25b97b1db851f1b
jamicolta <julian.micolta@hotmail.com>
2022-12-12 00:33:56 -0500

    Se crea la pagina "acerca de mi"

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import render_post, post_detail
+from .views import render_post, post_detail, render_about
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', render_post, name='posts'),
-    path('<int:post_id>', post_detail, name='post_detail')
+    path('<int:post_id>', post_detail, name='post_detail'),
+    path('about/', render_about)
 ]
