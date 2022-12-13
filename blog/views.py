37e4fb7403f73a9d49aedec645e67e1d047afa49
jamicolta <julian.micolta@hotmail.com>
2022-12-13 03:50:02 -0500

    Se crea paginator para la seccion blog

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator
+from django.http import Http404
 
 # Create your views here.
 def render_post(request):
     posts = Post.objects.all()
-    return render(request, 'posts.html', {'posts': posts})
+    page = request.GET.get('page', 1)
+    try:
+        paginator = Paginator(posts, 3)
+        posts = paginator.page(page)
+    except:
+        raise Http404
+    return render(request, 'posts.html', {'posts': posts, 'paginator': paginator})
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
