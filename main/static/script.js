9dccf45eea4c2eb2932e0642c1d7506e5d107bf5
jamicolta <julian.micolta@hotmail.com>
2022-12-13 01:31:28 -0500

    Se implementa libreria "Isotope js" para la seccion blog

--- main/static/script.js
+++ main/static/script.js
@@ -2,3 +2,22 @@ window.addEventListener('scroll', () => {
     var header = document.querySelector('.navbar');
     header.classList.toggle('sticky', window.scrollY > 0);
 });
+
+// Init Isotope
+var $grid = $('.grid').isotope({
+  // Options
+});
+// Filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+// filter .metal AND .transition items
+$grid.isotope({ filter: '.metal.transition' });
+
+// filter .alkali OR .alkaline-earth items
+$grid.isotope({ filter: '.alkali, .alkaline-earth' });
+
+// Show all items
+$grid.isotope({ filter: '*' });
